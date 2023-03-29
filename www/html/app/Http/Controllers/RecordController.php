25f775a56f9e227753164b816ba30f6784d00cce
Kathy Ngai <kathyaudrey2001@gmail.com>
2023-03-29 20:11:30 +0800

    store(): request num of ppl compare with room capacity, add waitlist numbering; destroy(): update waitlist after deleting

--- www/html/app/Http/Controllers/RecordController.php
+++ www/html/app/Http/Controllers/RecordController.php
@@ -33,17 +33,39 @@ class RecordController extends Controller
      */
     public function store(Request $request): Response
     {
-        $records = DB::table('records')->get();
+        // $records = DB::table('records')->where([['roomId', '=', $request->roomId]])->get();
+        $records = DB::select('select * from records where roomId = :roomId and status >= 0 order by created_at', ['roomId' => $request->roomId]);
+        $rooms = DB::table('rooms')->where([['roomId', '=', $request->roomId]])->get();
+        $conflictRecord = null;
+        $maxWaiting = 0;
+
+        foreach ($rooms as $x) {
+            if ($request->numOfPeople > $x->capacity){
+                return response([ 'error' => 'Number of people exceed capacity of room'], Response::HTTP_CONFLICT);
+            }
+        }
+
         foreach ($records as $x) {
-            if ($x->roomId == $request->roomId) {
-                if (($request->timeFrom >= $x->timeFrom) && ($request->timeFrom < $x->timeTo) || ($request->timeTo > $x->timeFrom) && ($request->timeTo <= $x->timeTo)) {
-                    return response([ 'error' => 'Time Conflict'], Response::HTTP_CONFLICT);    //??
+            if ($this->checkConflictTime($request, $x) && $x->status ==0) {
+                // return response([ 'error' => 'Time Conflict'], Response::HTTP_CONFLICT); 
+                $conflictRecord = $x;
+                break;
+            }
+        }
+
+        if ($conflictRecord != null) {
+            foreach ($records as $x) {
+                if ($this->checkConflictTime($x, $conflictRecord)) {
+                    $maxWaiting += 1;
                 }
             }
         }
+        $request->status = $maxWaiting;
+
         try {
-            $record = Record::create($request->all());
-            return response([ 'success' => $record->id], Response::HTTP_CREATED);
+            DB::insert('insert into records (id, userId, roomId, numOfPeople, timeFrom, timeTo, status, created_at, updated_at) values (?, ?, ?, ?, ?, ?, ?, ?, ?)', 
+                        [$request->id, $request->userId, $request->roomId, $request->numOfPeople, $request->timeFrom, $request->timeTo, $request->status, NOW(), NOW()]);
+            return response([ 'success' => $request->id], Response::HTTP_CREATED);
         } catch (\Throwable $e) {
             // dd($e->errorInfo);
             $errorCode = $e->errorInfo[1];
@@ -91,6 +113,47 @@ class RecordController extends Controller
     public function destroy(Record $record): Response
     {
         $record->delete();
+        $conflictRecord = null;
+
+        $acceptedRecords = DB::select('select * from records where roomId = :roomId and status = 0', ['roomId' => $record->roomId]);
+        $WaitlistRecords = DB::select('select * from records where roomId = :roomId and status > 0 order by created_at', ['roomId' => $record->roomId]);
+        
+        if ($record->status !=0) {
+            foreach ($acceptedRecords as $x) {
+                if ($this->checkConflictTime($record, $x)) {
+                    $conflictRecord = $x;
+                    foreach ($WaitlistRecords as $w) {
+                        if ($this->checkConflictTime($w, $conflictRecord) && $w->status > $record->status) {
+                            $affected = DB::update('update records set status = ?, updated_at = NOW() where id = ?', [(($w->status)-1), $w->id]);
+                        }
+                    }
+                }
+            }
+        } else {
+            $acceptedRecords = DB::select('select * from records where roomId = :roomId and status = 0', ['roomId' => $record->roomId]);
+            $conflicted = false;
+            $allocated = false;
+            foreach ($WaitlistRecords as $x) {
+                $conflicted = false;
+                if ($this->checkConflictTime($x, $record)) {
+                    if (!$allocated){
+                        foreach ($acceptedRecords as $a) {
+                            if ($this->checkConflictTime($x, $a)) {
+                                $conflicted = true;
+                                break;
+                            } 
+                        }
+                        if (!$conflicted) {
+                            $affected = DB::update('update records set status = ?, updated_at = NOW() where id = ?', [0, $x->id]);
+                            $allocated = true;
+                        }
+                    } else {
+                        $affected = DB::update('update records set status = ?, updated_at = NOW() where id = ?', [($x->status)-1, $x->id]);
+                    }
+                }
+            }
+        }
+
         return response([ 'success' => 'Record deleted'], Response::HTTP_OK);
     }
 
@@ -130,5 +193,9 @@ class RecordController extends Controller
             ['status', '>', 0]
         ])->get();
         return response($records, Response::HTTP_OK);
+    }
+
+    private function checkConflictTime($new, $old): bool {
+        return (($new->timeFrom >= $old->timeFrom) && ($new->timeFrom < $old->timeTo) || ($new->timeTo > $old->timeFrom) && ($new->timeTo <= $old->timeTo));
     }
 }
--- www/html/app/Models/Record.php
+++ www/html/app/Models/Record.php
@@ -16,5 +16,6 @@ class Record extends Model
         'numOfPeople',
         'timeFrom',
         'timeTo',
+        'status',
     ];
 }
